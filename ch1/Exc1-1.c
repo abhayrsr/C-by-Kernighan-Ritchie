dc2c465e6572681a70c88648120e1f32b7b9fcbc
abhayrsr <abhayrsr@LAPTOP-15N45SJ7>
2024-09-27 19:28:56 +0530

    solved ex1.1 ex1.2

---
+++ ch1/Exc1-1.c
@@ -0,0 +1,8 @@
+/*  Exercise 1-1. Run the 'hello, world' program on your system. Experiment with leaving out parts of the program, to see what error message you get.*/
+
+
+#include <stdio.h>
+
+int main(){
+	printf("hello, world");
+}
---
+++ ch1/Exc1-2.c
@@ -0,0 +1,9 @@
+/*Experiment to find out what happens when printf's argument string contains \c, where c is some character not listing above.*/
+
+#include <stdio.h>
+
+int main(){
+	printf("display a, \a");
+	printf("display v, \v");
+	printf("display r, \r");
+}
